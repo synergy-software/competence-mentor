0e920012ba91d55d41b9949ed943e8834b9f36ed
Cezary Piątek <cepi@ailleron.com>
2017-07-21 14:22:49 +0200

    Enable CORS

--- Web/Controllers/CompetenceController.cs
+++ Web/Controllers/CompetenceController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Model.Competence.Domain;
 using Model.Competence.Infrastructure;
 
 namespace Web.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CompetenceController : ApiController
     {
         // GET api/competence/{id}
