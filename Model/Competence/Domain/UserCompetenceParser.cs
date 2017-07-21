11a3ede15b3920945543c44795063824cdc9d12f
wosw <wojciech.swieboda@ailleron.com>
2017-07-21 16:46:48 +0200

    w

--- Model/Competence/Domain/UserCompetenceParser.cs
+++ Model/Competence/Domain/UserCompetenceParser.cs
@@ -9,11 +9,11 @@ namespace Model.Competence.Domain
         public string[] ParseCompetenceText(string compentenceText)
         {
             List<string> result = new List<string>();
-            var regex = new Regex(@"\S*#(?:\[[^\]]+\]|\S+)");
+            var regex = new Regex(@"\B#\w*[a-zA-Z-_+=!@#$%^&*.]+\w*");
             foreach (Match match in regex.Matches(compentenceText))
             {
                 var value = match.Value.Substring(1).ToLower();
-                value = value.TrimEnd(',');
+                value = value.TrimEnd(',', '.');
                 result.Add(value);
             }
 
