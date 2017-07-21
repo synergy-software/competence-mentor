ac42ae2760ab17ee3c6335c29aece8d0f8fc5928
Cezary Piątek <cepi@ailleron.com>
2017-07-21 16:42:25 +0200

    Add mocked aggregation graph. Integrate search module with api

--- UI/app.js
+++ UI/app.js
@@ -1,7 +1,57 @@
 var apiRoot = "http://localhost:2392/api/";
 var userId = 55;
 Vue.component('aggregations', {
-  template:'#Aggregations'
+  template:'#Aggregations',
+    mounted: function(){
+    var diameter = 500; //max size of the bubbles
+    var color    = d3.scale.category20c(); //color category
+
+    var bubble = d3.layout.pack()
+        .sort(null)
+        .size([diameter, diameter])
+        .value(function(d){return d.size})
+        .padding(1.5);
+
+    var svg = d3.select("#Graph")        
+        .attr("width", diameter)
+        .attr("height", diameter)
+        .attr("class", "bubble");
+   
+
+    //bubbles needs very specific format, convert data to this.
+    var nodes = bubble.nodes({children:[
+      {name:"java", size:5},
+      {name:"js", size:10},
+      {name:"c#", size:15},
+      {name:"elastic", size:20},
+    ]}).filter(function(d) { return !d.children; });
+
+    //setup the chart
+    var bubbles = svg.append("g")
+        .attr("transform", "translate(0,0)")
+        .selectAll(".bubble")
+        .data(nodes)
+        .enter();
+
+    //create the bubbles
+    bubbles.append("circle")
+        .attr("r", function(d){ return d.r; })
+        .attr("cx", function(d){ return d.x; })
+        .attr("cy", function(d){ return d.y; })
+        .style("fill", function(d) { return color(d.value); });
+
+    //format the text for each bubble
+    bubbles.append("text")
+        .attr("x", function(d){ return d.x; })
+        .attr("y", function(d){ return d.y + 5; })
+        .attr("text-anchor", "middle")
+        .text(function(d){ return d.name; })
+        .style({
+            "fill":"white", 
+            "font-family":"Helvetica Neue, Helvetica, Arial, san-serif",
+            "font-size": "12px"
+        });
+  }, 
 });
 
 Vue.component('search', {
@@ -10,22 +60,33 @@ Vue.component('search', {
    
     return {
       results:[
-        {name:"cepi"},
-        {name:"mace"},
-        {name:"wosw"}
-      ]
+       
+      ],
+      term:"",
+      showNoResults:false     
     };
   },
   methods:{
     search:function(){
-      this.results.push({name:"xxxx"});
+      var that = this;
+      $.ajax({
+        url: apiRoot+"competence/search?compentence="+this.term,
+        method:"GET",
+        contentType:'application/json',
+        
+      }).done(function(result){
+        that.results.length = 0;
+        that.showNoResults = result.length == 0;
+        result.forEach(function(item){
+          that.results.push({name:item})
+        });
+      });
     }
   }
 });
 Vue.component('profile', {
 
   template: '#Profile',
-
   props: [],
   data: function () {
      this.loadData();
@@ -48,7 +109,7 @@ Vue.component('profile', {
         })
       }).done(function(){
         that.loadData();
-      })
+      });
     },
     loadProfile: function () {
 
