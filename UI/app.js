d2e5f611037f16766fcd2dbbd0a01eefb972b0f3
Cezary Piątek <cepi@ailleron.com>
2017-07-21 12:14:07 +0200

    Add tags and components

--- UI/app.js
+++ UI/app.js
@@ -1,11 +1,41 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
+Vue.component('profile', {
+
+  template: '#Profile',
+
+  props: [],
+  data: function () {
+    return {
+      message: 'Moje ulubione technologie to #c# #java oraz #elasticsearch',
+      competences: [{
+        id: "ss",
+        text: "c#"
+      }, {
+        id: "xx",
+        text: "Java"
+      }, {
+        id: "aa",
+        text: "elasticsearch"
+      }]
+    };
   },
-  methods:{
-    update:function(){
+  methods: {
+    update: function () {
       alert(this.message);
+    },
+    loadProfile: function () {
+
+    },
+    loadSearch: function () {
+
+    },
+    loadAggregations: function () {
+
     }
   }
+});
+
+
+var app = new Vue({
+  el: '#app'
+
 })
